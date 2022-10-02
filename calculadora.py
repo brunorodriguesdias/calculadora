9ffa52ed7d20b5d4fe34411e4b6b2e175977aea8
Bruno <brunorodriguesdias@gmail.com>
2022-10-02 15:46:37 -0300

    testando elementos GUI

--- calculadora.py
+++ calculadora.py
@@ -3,5 +3,7 @@ from tkinter import *
 janela =Tk()
 janela.title('Calculadora')
 janela.geometry('380x460')
+label_1 = Label (janela, width = 20, text = "nome")
+label_1.place(x=10, y=10)
 
 janela.mainloop()
